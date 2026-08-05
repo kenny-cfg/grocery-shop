8695c280b240f0e5af173bb901d05fd41be482d9
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-05 20:28:27 +0100

    My first for loop

---
+++ loops.js
@@ -0,0 +1,12 @@
+// 1 - Initializes a variable to 0
+// 2 - Runs code inside the loop
+// 3 - Applies the third clause (i++)
+// 4 - Checks the second condition
+//   * if true, go to 2
+//   * if false, we are done
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
