16f2e3cb8df5ec4b4a595e584e94ccfec979f066
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-05 19:40:22 +0100

    Example

---
+++ index.js
@@ -0,0 +1,12 @@
+let age = 5;
+let message;
+
+if (age >= 18) {
+  message = "Welcome to the website";
+} else if (age >= 13) {
+  message = "You can buy a limited amount of stuff on this website";
+} else {
+  message = "You are way too young, go away";
+}
+
+console.log(message);
