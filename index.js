fb0ca7446b4bfbd940ebed2b34c4afeb69a58150
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-05 20:10:48 +0100

    Working age example

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let age = 19;
+let age = 10;
+let hasParentalConsent = false;
 let message;
 
 if (age >= 18) {
   message = "Welcome to the website";
 } else if (age >= 13) {
   message = "You can buy a limited amount of stuff on this website";
+} else if (age >= 12 && hasParentalConsent) {
+  message = "You are allowed to access the website with parental consent";
+} else if (age >= 12 && !hasParentalConsent) {
+  message = "You need parental consent to access this website";
 } else {
   message = "You are way too young, go away";
 }
