a4e5d4c6834469e1c783f9392c23c1d3c9c6d826
Kenny Hung (he) <kenny.cfg@outlook.com>
2026-08-06 18:55:01 +0100

    Looping through an array

--- loops.js
+++ loops.js
@@ -5,8 +5,16 @@
 //   * if true, go to 2
 //   * if false, we are done
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+const fruits = ["apple", "banana", "kiwi", "avocado", "tomato"];
+
+// for (let index = 0; index < fruits.length; index ++) {
+//   console.log(fruits[index]);
+// }
+
+for (const fruit of fruits) {
+  console.log(fruit);
 }
-
-
